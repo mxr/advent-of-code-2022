c08aaf83e544d7960555a57e4b853f759bb79c2c
Max R <mxr@users.noreply.github.com>
2022-12-01 00:06:17 -0500

    solve

---
+++ day01.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import functools
+
+def parse(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+
+@functools.lru_cache(maxsize=None)
+def helper(filename: str) -> list[int]:
+    return [sum(int(cal) for cal in elf.splitlines()) for elf in parse(filename).split('\n\n')]
+
+
+def part1(filename: str) -> int:
+    inp = parse(filename).split('\n\n')
+    return max(helper(filename))
+
+
+def part2(filename: str) -> int:
+    return sum(sorted(helper(filename),reverse=True)[:3])
+
+
+
+if __name__ == "__main__":
+    from _common import main
+
+    raise SystemExit(main(part1, part2))
